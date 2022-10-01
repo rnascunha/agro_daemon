bbca83de5b4d800c7101631e9891cbd7d5c7d56f
Rafael Cunha <rnascunha@gmail.com>
2022-10-01 20:45:17 -0300

    Adding conan recipe

---
+++ conanfile.py
@@ -0,0 +1,41 @@
+from conans import ConanFile, CMake, tools
+
+class AgroDaemonConan(ConanFile):
+    name = "agro_daemon"
+    version = "0.1"
+    license = "MIT"
+    author = "Rafael Cunha <rnascunha@gmail.com>"
+    url = "https://github.com/rnascunha/agro_daemon"
+    description = "Agro Project Daemon"
+    topics = ("agro", "daemon", "project")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False], "ssl": [True, False], "verbose": [0, 1, 2, 3, 4, 5]}
+    default_options = {"shared": False, "fPIC": True, "ssl": False, "verbose": 4, "boost:header_only": True}
+    generators = "cmake_find_package"
+    requires = "argh/1.3.2", "sqlite3/3.39.4", "boost/1.80.0", "openssl/1.1.1q", "rapidjson/cci.20220822", "tree_trunks/0.1@base/stable", "coap-te/0.1@base/stable", "pusha/0.1@base/stable"
+    exports = "LICENSE", "README.md", "URL.txt", "db/scheme.sql"
+    exports_sources = "*.c", "*.cpp", "*.h", "*.hpp", "*.hpp.in", "*CMakeLists.txt", "*.cmake"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.parallel = False
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy("agro_daemon", dst="", src="bin")
+        self.copy("init_db", dst="", src="bin")
+        self.copy("scheme.sql", dst="", src="db")
+
+    def package(self):
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*.c", dst="include", src="src")
+        self.copy("*.hpp", dst="src", src="src")
+        self.copy("*.cpp", dst="src", src="src")
+        self.copy("agro_daemon", dst="bin")
+        self.copy("init_db", dst="bin")
+        self.copy("scheme.sql", dst="bin", src="db")
