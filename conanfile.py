4b911a47d56fb36e6ac712545f0d8f068499422e
Rafael Cunha <rnascunha@gmail.com>
2022-10-03 14:36:11 -0300

    Backing up to adpat to Windows

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake
+from conans.model.version import Version
 
 class AgroDaemonConan(ConanFile):
     name = "agro_daemon"
@@ -12,9 +13,13 @@ class AgroDaemonConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False], "ssl": [True, False], "verbose": [0, 1, 2, 3, 4, 5]}
     default_options = {"shared": False, "fPIC": True, "ssl": False, "verbose": 4, "boost:header_only": True}
     generators = "cmake_find_package"
-    requires = "argh/1.3.2", "sqlite3/3.39.4", "boost/1.80.0", "openssl/1.1.1q", "rapidjson/cci.20220822", "tree_trunks/0.1@base/stable", "coap-te/0.1@base/stable", "pusha/0.1@base/stable"
+    requires = "argh/1.3.2", "sqlite3/[>3.0.0]", "boost/[>=1.70.0]", "openssl/1.1.1q", "rapidjson/cci.20220822", "tree_trunks/[>=0.1]@base/stable", "coap-te/[>=0.1]@base/stable", "pusha/[>=0.1]@base/stable"
     exports = "LICENSE", "README.md", "URL.txt", "db/scheme.sql"
     exports_sources = "*.c", "*.cpp", "*.h", "*.hpp", "*.hpp.in", "*CMakeLists.txt", "*.cmake"
+
+    def build_requirements(self):
+        if CMake.get_version() < Version("3.10"):
+            self.tool_requires("cmake/[>= 3.10]")
 
     def config_options(self):
         if self.settings.os == "Windows":
